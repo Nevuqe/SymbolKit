cc0ce1a2c39ca70384b5a6fb2bc04ce35e0b183c
jckgan <jack@nevuqe.com>
2023-03-30 10:21:41 +0100

    remove iconoir.com from source

--- iconoir.com/lib/getHeaderProps.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export function getHeaderProps() {
-  const packageJson = JSON.parse(fs.readFileSync('../package.json').toString());
-  return {
-    currentVersion: `v${packageJson.version}`,
-  };
-}
